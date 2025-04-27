f03dadd2e90b8e1c0f23610f493484a45c5906b1
Park-SM <park97.sm@gmail.com>
2025-04-27 18:54:51 +0900

    build.gradle 공통화

--- common/design/build.gradle.kts
+++ common/design/build.gradle.kts
@@ -5,32 +5,8 @@ plugins {
 
 android {
     namespace = "com.smparkworld.common.design"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
+apply(from = rootProject.file("core-android-build.gradle"))
 
 dependencies {
 
